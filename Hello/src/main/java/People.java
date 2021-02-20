c27833c1b42085949531a55c7c6f69da30d1a831
jionjion <jionjion@aliyun.com>
2021-02-20 14:44:38 +0800

    2021-02-20

---
+++ Hello/src/main/java/People.java
@@ -0,0 +1,21 @@
+/**
+ * 正常的Java类
+ *
+ * @author Jion
+ */
+public class People {
+
+    private final String name;
+
+    public People(String name) {
+        this.name = name;
+    }
+
+    public void sayHello() {
+        System.out.println("Hello" + name);
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ Hello/src/main/kotlin/main.kt
@@ -0,0 +1,8 @@
+/**
+ *  使用 Kotlin
+ */
+fun main() {
+    println("What's your name?")
+    val name = readLine()
+    println("Hello $name!")
+}
